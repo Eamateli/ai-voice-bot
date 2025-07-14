6f19e8667ffae8a0990c00d566db72494a6dd864
Eamateli <e.a.mateli@gmail.com>
2025-07-14 18:39:52 +0300

    Add new endpoing, post

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.services.cohere_service import test_cohere
+from app.models.schemas import ChatRequest, ChatRespose
 
 app= FastAPI()
 
@@ -11,3 +12,10 @@ async def root():
 async def test_ai_endpoint():
     ai_response = test_cohere()
     return {"ai_says": ai_response}
+
+@app.post("/api/v1/chat", response_model=ChatRespose)
+async def chat(request: ChatRequest):
+    return ChatRespose (
+        response=f"You said: {request.message}"
+        token_used=10
+    )
